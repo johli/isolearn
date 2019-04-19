53e66f41fab026ccd270a6768ee5d500f926f8cf
Johannes Linder <jlinder2@cs.washington.edu>
2019-04-19 15:57:49 -0700

    Initial commit of isolearn package.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # isolearn
+
+Automated loading and processing of genomics data for use in deep learning models (Keras).
+Implements Keras data generators for loading and encoding Pandas dataframes and RNA-Seq count matrices into numerical tensors.
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="isolearn",
+    version="0.1",
+    author="Johannes Linder",
+    author_email="johannes.linder@hotmail.com.com",
+    description="Keras Genomics Data Generators",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/johli/isolearn",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
